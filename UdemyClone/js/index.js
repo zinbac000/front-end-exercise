f298662219cf84a1bbc8d2a7427a5eb833adb5fd
zinbac000@gmail.com <zinbac000@gmail.com>
2020-07-02 20:12:15 +0700

    add instructor section for udemy layout

--- UdemyClone/js/index.js
+++ UdemyClone/js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(".courses__slider").slick({
+  $(".courses__slider, .testimonial__slider").slick({
     mobileFirst: true,
     dots: false,
     arrows: false,
@@ -14,19 +14,21 @@ $(function () {
         breakpoint: 767,
         settings: {
           slidesToShow: 3,
-          arrows: true
+          arrows: false
         }
       },
       {
         breakpoint: 991,
         settings: {
-          slidesToShow: 4
+          slidesToShow: 4,
+          arrows: true
         }
       },
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 5
+          slidesToShow: 5,
+          arrows: true
         }
       }
     ]
@@ -34,5 +36,10 @@ $(function () {
 
   $(".courses__slider.collapse").on("shown.bs.collapse", function () {
     $(this).slick("setPosition");
+  });
+
+  $(".courses__desktop .nav-link").on("shown.bs.tab", function () {
+    let targetTab = $(this).attr("href");
+    $(targetTab).slick("setPosition");
   });
 });
